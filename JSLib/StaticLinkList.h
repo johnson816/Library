5bdd395b575e49bf4e46e02b70fed41fb881ad9c
johnson816 <1349266602@qq.com>
2020-10-23 18:32:42 +0800

    add StaticLinkList.h

---
+++ JSLib/StaticLinkList.h
@@ -0,0 +1,77 @@
+#ifndef STATICLINKLIST_H
+#define STATICLINKLIST_H
+
+#include "LinkList.h"
+
+namespace JSLib
+{
+
+template <typename T, int N>
+class StaticLinkList : public LinkList<T>
+{
+protected:
+    typedef typename LinkList<T>::Node Node;
+    struct SNode : public Node
+    {
+        void* operator new(unsigned int size,void* local)
+        {
+            (void) size;
+            return local;
+        }
+    };
+
+    unsigned char m_space[sizeof(SNode) * N];
+    int m_used[N];
+
+    Node* creat()
+    {
+        SNode* ret = NULL;
+        for(int i=0;i<N;i++)
+        {
+            if(!m_used[i])
+            {
+                ret = reinterpret_cast<SNode*>(m_space) + i;
+                ret = new(ret)SNode();
+                m_used[i] = 1;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    void destroy(Node* pn)
+    {
+        SNode* tem = reinterpret_cast<SNode*>(m_space);
+        SNode* pd = dynamic_cast<SNode*>(pn);
+        for(int i=0;i<N;i++)
+        {
+            if(pd == (tem + i))
+            {
+                m_used[i] = 0;
+                pd->~SNode();
+            }
+        }
+    }
+public:
+    StaticLinkList()
+    {
+        for(int i=0;i<N;i++)
+        {
+            m_used[i] = 0;
+        }
+    }
+    int capacity()
+    {
+        return N;
+    }
+    ~StaticLinkList()
+    {
+        StaticLinkList::clear();
+    }
+};
+
+}
+
+
+
+#endif // STATICLINKLIST_H
--- JSLib/main.cpp
+++ JSLib/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "LinkList.h"
+#include "StaticLinkList.h"
 
 using namespace std;
 using namespace JSLib;
@@ -8,15 +8,7 @@ using namespace JSLib;
 
 int main()
 {
-    LinkList<int>list;
-    for(int i=0;i<5;i++)
-    {
-        list.insert(i);
-    }
 
-    for(list.move(0);!list.end();list.next())
-    {
-        cout << list.current() << endl;
-    }
+
     return 0;
 }
