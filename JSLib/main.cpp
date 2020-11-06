cfb2ea20b0881191a222f7d03abce9fb33b06cd7
johnson816 <1349266602@qq.com>
2020-11-06 22:52:41 +0800

    use Stack solve the problem of symbol matching

--- JSLib/main.cpp
+++ JSLib/main.cpp
@@ -1,11 +1,82 @@
 #include <iostream>
-#include "DualCircleList.h"
+#include "LinkStack.h"
 
 using namespace std;
 using namespace JSLib;
 
+bool is_left(char c)
+{
+    return (c == '{') || (c == '[') || (c == '<') || (c == '(');
+}
+
+bool is_right(char c)
+{
+    return (c == '}') || (c == ']') || (c == '>') || (c == ')');
+}
+
+bool is_quot(char c)
+{
+    return (c == '\'') || (c == '\"');
+}
+
+bool is_match(char l,char r)
+{
+    return ((l == '{') && (r== '}')) ||
+           ((l == '[') && (r== ']')) ||
+           ((l == '<') && (r== '>')) ||
+           ((l == '(') && (r== ')')) ||
+           ((l == '\'') && (r== '\'')) ||
+           ((l == '\"') && (r== '\"')) ;
+
+}
+
+bool scan(const char* code)
+{
+    LinkStack<char>stack;
+    int i= 0;
+    bool ret = true;
+
+    code = (code == NULL) ? "" : code;
+
+    while(ret && (code[i] != '\0'))
+    {
+        if(is_left(code[i]))
+        {
+            stack.push(code[i]);
+        }
+        else if(is_right(code[i]))
+        {
+            if((stack.size() > 0) && is_match(stack.top(),code[i]))
+            {
+                stack.pop();
+            }
+            else
+            {
+                ret = false;
+                break;
+            }
+        }
+        else if(is_quot(code[i]))
+        {
+            if(stack.size() == 0 || !is_match(stack.top(),code[i]))
+            {
+                stack.push(code[i]);
+            }
+            else if(is_match(stack.top(),code[i]))
+            {
+                stack.pop();
+            }
+        }
+        i++;
+    }
+    return ret && (stack.size() == 0);
+}
+
 int main()
 {
+
+    cout << scan("else if(is_quot(code[i])){if(stack.size() == 0 || !is_match(stack.top(),code[i])){stack.push(code[i]);}else if(is_match(stack.top(),code[i])){stack.pop();}}") << endl;
+
 
     return 0;
 }
